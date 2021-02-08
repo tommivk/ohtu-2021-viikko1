4bf4b161a36b7dcb329cd44b9c4715d5a8ba1531
Tommi <tommikokk@gmail.com>
2021-02-08 11:03:52 +0200

    week 2 exercise 2

--- src/main/java/ohtu/ohtuvarasto/Varasto.java
+++ src/main/java/ohtu/ohtuvarasto/Varasto.java
@@ -10,28 +10,15 @@ public class Varasto {
     public Varasto(double tilavuus) {  // tilavuus on annettava
         if (tilavuus > 0.0) {
             this.tilavuus = tilavuus;
-        } else // virheellinen, nollataan
-        {
+        } else {
             this.tilavuus = 0.0;  // => käyttökelvoton varasto
         }
         saldo = 0;     // oletus: varasto on tyhjä
     }
 
     public Varasto(double tilavuus, double alkuSaldo) { // kuormitetaan
-        if (tilavuus > 0.0) {
-            this.tilavuus = tilavuus;
-        } else // virheellinen, nollataan
-        {
-            this.tilavuus = 0.0;  // => käyttökelvoton varasto
-        }
-        if (alkuSaldo < 0.0) {
-            this.saldo = 0.0;
-        } else if (alkuSaldo <= tilavuus) // mahtuu
-        {
-            this.saldo = alkuSaldo;
-        } else {
-            this.saldo = tilavuus;  // täyteen ja ylimäärä hukkaan!
-        }
+        setTilavuus(tilavuus);
+        setSaldo(alkuSaldo, tilavuus);
     }
 
     // --- ottavat aksessorit eli getterit: ---
@@ -39,8 +26,27 @@ public class Varasto {
         return saldo;
     }
 
+    public void setSaldo(double alkuSaldo, double tilavuus) {
+        if (alkuSaldo < 0.0) {
+            this.saldo = 0.0;
+        } else if (alkuSaldo <= tilavuus) {
+            this.saldo = alkuSaldo;
+        } 
+        else {
+            this.saldo = tilavuus;  // täyteen ja ylimäärä hukkaan!
+        }
+    }
+
     public double getTilavuus() {
         return tilavuus;
+    }
+
+    public void setTilavuus(double tilavuus) {
+        if (tilavuus > 0.0) {
+            this.tilavuus = tilavuus;
+        } else {
+            this.tilavuus = 0.0;  // => käyttökelvoton varasto
+        }
     }
 
     public double paljonkoMahtuu() {  // huom: ominaisuus voidaan myös laskea
@@ -49,12 +55,10 @@ public class Varasto {
 
     // --- asettavat aksessorit eli setterit: ---
     public void lisaaVarastoon(double maara) {
-        if (maara < 0) // virhetilanteessa voidaan tehdä 
-        {
+        if (maara < 0) {
             return;       // tällainen pikapoistuminenkin!
         }
-        if (maara <= paljonkoMahtuu()) // omia aksessoreita voi kutsua
-        {
+        if (maara <= paljonkoMahtuu()) {
             saldo = saldo + maara;          // ihan suoraan sellaisinaan
         } else {
             saldo = tilavuus;  // täyteen ja ylimäärä hukkaan!
@@ -62,8 +66,7 @@ public class Varasto {
     }
 
     public double otaVarastosta(double maara) {
-        if (maara < 0) // virhetilanteessa voidaan tehdä 
-        {
+        if (maara < 0) {
             return 0.0;   // tällainen pikapoistuminenkin!
         }
         if (maara > saldo) {          // annetaan mitä voidaan
--- src/main/java/paaohjelma/Main.java
+++ src/main/java/paaohjelma/Main.java
@@ -55,5 +55,6 @@ public class Main {
         saatiin = mehua.otaVarastosta(-32.9);
         System.out.println("saatiin " + saatiin);
         System.out.println("Mehuvarasto: " + mehua);
+
     }
 }
